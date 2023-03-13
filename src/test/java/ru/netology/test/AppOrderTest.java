b83f1206e670a6d4f1c3d5e9927dbb4bfa64c7bf
Alina Prish <alinaprish92@mail.ru>
2023-03-13 19:00:17 +0300

    add hw right

--- src/test/java/ru/netology/test/AppOrderTest.java
+++ src/test/java/ru/netology/test/AppOrderTest.java
@@ -12,14 +12,10 @@ class AppOrderTest {
     @Test
     void shouldTest () {
         open("http://localhost:7777");
-        SelenideElement form = $("[.form]");
-        form.$("[.input data-test-id=name]").setValue("Екатерина");
-        form.$("[.input data-test-id=phone]").setValue("+79105465454");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
-        $("[paragraph data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
-
-
+        $("[data-test-id='name'] input").setValue("Екатерина");
+        $("[data-test-id='phone'] input").setValue("+79105465454");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 }
 }
