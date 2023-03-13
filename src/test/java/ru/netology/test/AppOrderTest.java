d6cd4b51788f40442093ee60e51ed2a4363442e2
Alina Prish <alinaprish92@mail.ru>
2023-03-13 18:14:55 +0300

    add revision

--- src/test/java/ru/netology/test/AppOrderTest.java
+++ src/test/java/ru/netology/test/AppOrderTest.java
@@ -1,31 +1,23 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.SelenideElement;
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
-import org.junit.jupiter.api.Test;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
 
-import javax.annotation.Nonnull;
-
-import static com.codeborne.selenide.Selenide.;
-//@Nonnull;
-//@CanIgnoreReturnValue;
-
-import java.nio.channels.ConnectionPendingException;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
 
 
 class AppOrderTest {
     @Test
     void shouldTest () {
-        open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=AppOrder-form]");
-        form.$("[data-test-id=name]").setValue("Екатерина");
-        form.$("[data-test-id=phone]").setValue("+79105465454");
+        open("http://localhost:7777");
+        SelenideElement form = $("[.form]");
+        form.$("[.input data-test-id=name]").setValue("Екатерина");
+        form.$("[.input data-test-id=phone]").setValue("+79105465454");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
+        $("[paragraph data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
 
 
